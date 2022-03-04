d95d8cf1e8be14a0cd5fc3e789a108e5b25dc735
David Maxwell <maxwelld90@gmail.com>
2022-03-04 16:30:53 +0000

    Updated settings to include Postgres

--- api/api/settings.py
+++ api/api/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 if os.getenv('MEDICINE_ENVIRONMENT') == 'production':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.getenv('MEDICINE_DB_NAME'),
             'USER': os.getenv('MEDICINE_DB_USER'),
             'PASSWORD': os.getenv('MEDICINE_DB_PASSWORD'),
             'HOST': '127.0.0.1',
             'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
