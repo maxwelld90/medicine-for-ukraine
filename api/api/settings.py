938fbc158428ce3c7733e67813e2f0f5036d756f
David Maxwell <maxwelld90@gmail.com>
2022-03-04 16:48:10 +0000

    Updated settings to alter JSON view

--- api/api/settings.py
+++ api/api/settings.py
@@ -144,3 +144,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getenv('MEDICINE_DEBUG') != 'true':
+    REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        )
+    }
