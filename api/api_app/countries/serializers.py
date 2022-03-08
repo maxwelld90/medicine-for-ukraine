0fda989d2a52ba8d74d82f7101cc0b39fad0b501
David Maxwell <maxwelld90@gmail.com>
2022-03-08 11:28:14 +0000

    Added code to country API listing

--- api/api_app/countries/serializers.py
+++ api/api_app/countries/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from api_app.models import Address, Country
 
 class CountrySerializer(serializers.ModelSerializer):
+    code = serializers.CharField(max_length=5)
     name = serializers.CharField(max_length=256)
     flag_url = serializers.CharField(max_length=256)
 
     class Meta:
         model = Country
-        fields = ['name', 'flag_url']
+        fields = ['code', 'name', 'flag_url']
 
 class AddressSerializer(serializers.ModelSerializer):
     address_lines = serializers.CharField()
