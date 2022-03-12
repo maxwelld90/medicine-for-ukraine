a06d534ba9d9d1b57eea76ed027cd5140ad0ef07
David Maxwell <maxwelld90@gmail.com>
2022-03-12 16:31:19 +0000

    Updated API save callback; IP can be pulled from server

--- api/api_app/save/views.py
+++ api/api_app/save/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from api_app.items.medicine_reader import call_medicine_reader
 from api_app.save import request_parser
 
 
