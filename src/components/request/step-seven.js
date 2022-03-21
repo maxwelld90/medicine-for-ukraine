a69e5a07418a1d447d18717ed98eaffff37963e1
Natalia Diedyk <natalja.dj@gmail.com>
2022-03-21 12:59:59 +0200

    Style address box and remove duplicated styles

--- src/components/request/step-seven.js
+++ src/components/request/step-seven.js
@@ -1,18 +1,18 @@
-import React, {useContext, useEffect, useState} from "react";
-import {useTranslation} from "react-i18next";
+import React, { useContext, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import ImageLoader from "../imageLoader";
-import {RequestContext} from "./request-context";
-import {fetchAddress, fetchItems} from "../../api";
+import { RequestContext } from "./request-context";
+import { fetchAddress } from "../../api";
 
 // Check if each store has at least one file
 const isValidRequest = (request) => {
   return Object.values(request.stores).every((s) => {
     return s.screenshots && s.screenshots.length > 0;
   });
-}
+};
 
-export default function StepSeven({onNext}) {
+export default function StepSeven({ onNext }) {
   const [request] = useContext(RequestContext);
   const [isCompletedStep, setIsCompletedStep] = useState(false);
   const [t] = useTranslation(["translation", "common"]);
@@ -22,26 +22,24 @@ export default function StepSeven({onNext}) {
       request.stores[index].screenshots = files;
       setIsCompletedStep(isValidRequest(request));
     };
-  }
-
+  };
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [address, setAddress] = useState({});
 
-  useEffect( () => {
-    fetchAddress(request.countryCode)
-      .then(
-        (result) => {
-          setAddress(result);
-          request.addressId = result.id;
-          setIsLoaded(true);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
+  useEffect(() => {
+    fetchAddress(request.countryCode).then(
+      (result) => {
+        setAddress(result);
+        request.addressId = result.id;
+        setIsLoaded(true);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
   }, [request.countryCode]);
 
   if (error) {
@@ -59,20 +57,23 @@ export default function StepSeven({onNext}) {
 
       <p className="multilingual en">{t("common:STEP_SEVEN.FIRST_LINE")}</p>
 
-      <div className={'address-text'}>{address.address_lines}</div>
+      <div className="address-text">{address.address_lines}</div>
 
       <p className="multilingual en">{t("common:STEP_SEVEN.SECOND_LINE")}</p>
 
-      <ul className={'file-list'}>
+      <ul className={"file-list"}>
         {Object.entries(request.stores).map(([i, store]) => (
           <li key={i}>
             <span>{store.store_domain}</span>
-            <ImageLoader onUpload={getOnUploadHandler(i)} existingFiles={store.screenshots}/>
+            <ImageLoader
+              onUpload={getOnUploadHandler(i)}
+              existingFiles={store.screenshots}
+            />
           </li>
         ))}
       </ul>
 
-      <div className={'btn-wrap'}>
+      <div className={"btn-wrap"}>
         <button disabled={!isCompletedStep} onClick={onNext}>
           {t("common:NEXT_BUTTON")}
         </button>
