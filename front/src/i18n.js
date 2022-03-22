bfc6176f3272fa0b7c57fcca8578a605206d2180
David Maxwell <maxwelld90@gmail.com>
2022-03-22 14:46:52 +0000

    Languages now pulled from LANGUAGES.json in frontend app

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -2,11 +2,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import availableLanguages from './LANGUAGES.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const languages = ["en", "es", "pl", "de"];
+const languages = Object.keys(availableLanguages);
 
 i18n
   /*
@@ -28,7 +29,6 @@ i18n
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
-
     react: {
       useSuspense: false
     }
