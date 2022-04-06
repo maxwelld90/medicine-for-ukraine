692ba8778bdf6c93112c76b61dc1ae725cde5555
Dmytro Pylypenko <dimpiax@gmail.com>
2022-04-06 18:55:23 +0200

    feat: update `Recipients` component with `useAsync` hook for a request

--- front/src/components/request/Recipients.js
+++ front/src/components/request/Recipients.js
@@ -1,20 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
+import { useAsync } from "react-use";
+import { useTranslation } from "react-i18next";
+
+import { fetchRecipients } from "../../api";
+
+import Loader from "../loader";
+import Error from "../error";
+
 import { RequestContext } from "./requestContext";
 import StepNavigation from "./components/StepNavigation";
 import StepDescription from "./components/StepDescription";
 
-import { useTranslation } from "react-i18next";
-import { fetchRecipients } from "../../api";
-import Loader from "../loader";
-import Error from "../error";
-
 export default function Recipients({ onNext, onBack, language }) {
+  const { loading, error, value } = useAsync(fetchRecipients);
+
   const [request, setRequest] = useContext(RequestContext);
   const [t] = useTranslation(["translation", "common"]);
-
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [recipients, setRecipients] = useState([]);
 
   const handleSelect = (recipient) => {
     setRequest({ ...request, recipientId: recipient.recipient_id });
@@ -22,24 +23,12 @@ export default function Recipients({ onNext, onBack, language }) {
     typeof onNext === "function" && onNext();
   };
 
-  useEffect(() => {
-    fetchRecipients().then(
-      (result) => {
-        setIsLoaded(true);
-        setRecipients(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    );
-  });
 
   return (
     <>
       {error && <Error />}
-      {!isLoaded && <Loader />}
-      {!error && isLoaded && (
+      {loading && <Loader />}
+      {!error && !loading && (
         <div>
           <StepDescription
             step="1/5"
@@ -48,7 +37,7 @@ export default function Recipients({ onNext, onBack, language }) {
           />
 
           <ul className="item-list countries direction">
-            {recipients.map((recipient, i) => (
+            {value.map((recipient, i) => (
               <li
                 key={i}
                 onClick={() => handleSelect(recipient)}
