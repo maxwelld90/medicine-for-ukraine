ce61f31509c956bd5ff6c165b2713d9f8657363d
David Maxwell <maxwelld90@gmail.com>
2022-04-06 12:17:52 +0100

    Updates to environment configuration

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,24 +1,15 @@
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path, include
 from medicine_api.admin import admin_site
+from django.http import HttpResponse, HttpResponseRedirect
 
 urlpatterns = [
     path('maintenance/', admin_site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('medicine_api.urls')),
 ]
+
+def handler404(request, exception):
+    """
+    A simple 404 handler to redirect the user to the main homepage.
+    """
+    return HttpResponseRedirect('https://medicineforukraine.org')
