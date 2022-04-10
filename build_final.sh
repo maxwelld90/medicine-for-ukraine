c7f8a90aec7e3549377638fea3cdea8ccb4f7171
David Maxwell <maxwelld90@gmail.com>
2022-04-10 17:59:54 +0100

    Updated Makefile

--- build_final.sh
+++ build_final.sh
@@ -8,6 +8,11 @@ then
     deactivate;
 
     cd ../;
+
+    mv ./landing/out/static/browserconfig.xml ./landing/out/browserconfig.xml;
+    mv ./landing/out/static/favicon.ico ./landing/out/favicon.ico;
+    mv ./landing/out/static/manifest.json ./landing/out/manifest.json;
+
     mkdir -p ../landing;
     rsync -r landing/out/ ../landing;
 fi
