2b8f0bba478f3a3ef705b67eeca537acee67e755
David Maxwell <maxwelld90@gmail.com>
2022-04-10 20:03:32 +0100

    Updated CRON file

--- cron.sh
+++ cron.sh
@@ -5,11 +5,16 @@
 # Processes LinkMetadata from the Google Sheets source.
 #
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <envfile_path>" >&2
+  exit 1
+fi
+
 echo "Launching Medicine for Ukraine CRON job...";
 
 [ ! -d "./virtualenvs/api/" ] && { echo "Virtual environment could not be found. Did you run make all?"; exit 1; }
 
-source ./env.prep.sh .env.development;
+source ./env.prep.sh $1;
 . virtualenvs/api/bin/activate;
 cd api;
 python manage.py cron_link_populator;
