38b2cc95e0e76d241a6c68e7615f81fb6b32f458
David Maxwell <maxwelld90@gmail.com>
2022-04-10 21:50:08 +0100

    Updated CRON

--- cron.sh
+++ cron.sh
@@ -12,8 +12,10 @@ fi
 
 echo "Launching Medicine for Ukraine CRON job...";
 
+[ ! -d "/srv/medicine-for-ukraine/git-repo/" ] && { echo "Are you running this script on the production server? Can't find path."; exit 1; }
 [ ! -d "/srv/medicine-for-ukraine/git-repo/virtualenvs/api/" ] && { echo "Virtual environment could not be found. Did you run make all?"; exit 1; }
 
+cd /srv/medicine-for-ukraine/git-repo;
 source ./env.prep.sh $1;
 . virtualenvs/api/bin/activate;
 cd api;
